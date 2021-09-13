789a23c15a0b1c746fa46c2dff439f3a0a1852ca
daniele-pipino <Danielemacpipino@icloud.com>
2021-09-13 13:11:17 +0200

    aggiunto voto da 1-5, piu fix flag img

--- src/components/Filmcard.vue
+++ src/components/Filmcard.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="filmcard">
     <p>{{ film.title }}</p>
-    <img :src="`@/assets/images/${this.language}.png`" alt="" />
+    <img :src="getFlag(film.original_language)" alt="" class="img-fluid" />
     <p>{{ film.original_language }}</p>
-    <p>{{ film.vote_average }}</p>
+    <p>{{ this.vote }}</p>
     <p class="copertina">{{ posterUrl }}</p>
   </div>
 </template>
@@ -16,11 +16,22 @@ export default {
     return {
       language: this.film.original_language,
       posterUrl: this.getPoster(),
+      vote: this.getVote(),
     };
   },
   methods: {
+    getFlag(lang) {
+      return require(`@/assets/images/${lang}.png`);
+    },
     getPoster() {
       return (this.posterUrl = `${this.posterBaseUri}w342${this.film.poster_path}`);
+    },
+    getVote() {
+      const initialVote = this.film.vote_average;
+      console.log("iniziale", initialVote);
+      const finalVote = Math.ceil(initialVote / 2);
+      console.log("finale", finalVote);
+      return finalVote;
     },
   },
   created() {},
--- src/components/Seriescard.vue
+++ src/components/Seriescard.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="seriescard">
     <p>{{ serie.name }}</p>
-    <img :src="this.getFlag" alt="" />
+    <img :src="getFlag(serie.original_language)" alt="" class="img-fluid" />
     <p>{{ serie.original_language }}</p>
-    <p>{{ serie.vote_average }}</p>
+    <p>{{ this.vote }}</p>
     <p>{{ posterUrl }}</p>
   </div>
 </template>
@@ -16,12 +16,20 @@ export default {
     return {
       language: this.serie.original_language,
       posterUrl: this.getPoster(),
+      vote: this.getVote(),
     };
   },
   methods: {
     getFlag: (language) => require(`@/assets/images/${language}.png`),
     getPoster() {
       return (this.posterUrl = `${this.posterBaseUri}w342${this.serie.poster_path}`);
+    },
+    getVote() {
+      const initialVote = this.serie.vote_average;
+      console.log("iniziale", initialVote);
+      const finalVote = Math.ceil(initialVote / 2);
+      console.log("finale", finalVote);
+      return finalVote;
     },
   },
 };
